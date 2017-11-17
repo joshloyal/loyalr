aff6ad1c7b5d69c36b23a75a175aad403b2bbd0f
Joshua Loyal <jloyal25@gmail.com>
2017-11-17 09:42:20 -0600

    exported get_n_jobs

--- R/parallel.R
+++ R/parallel.R
@@ -2,6 +2,12 @@
 #'
 #' Determines the number of jobs which are going to run in parallel.
 #' This function is inspired by the python function in joblib.
+#'
+#' @param n_jobs The maximum number of concurrently running jobs.
+#'   If -1 all CPUs are used. For n_jobs below -1, (n_cpus + 1 + n_jobs)
+#'   are used. Thus for n_jobs = -2, all CPUs but one are used.
+#'
+#' @export
 get_n_jobs <- function(n_jobs) {
   if (n_jobs == 0) {
     stop('n_jobs == 0 in parallel has no meaning.')
